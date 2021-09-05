c8cbe6bd476df99b83b0199206ec5d5ce1d48c1c
Kpokc <pavmakarovs@gmail.com>
2021-09-05 21:08:31 +0100

    Add query request

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_products(request):
     """ A view to return all products, including sorting and search """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name_incontains=query) | Q(description_incontains=query)
+            products = products.filter(queries)
 
     context = {
         'products' : products,
+        'search_term' : query,
     }
 
     return render(request, "products/products.html", context)
