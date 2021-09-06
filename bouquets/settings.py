ff4b7c252dc8ce9635d129c7107c89fcf2a4e9a4
Kpokc <pavmakarovs@gmail.com>
2021-09-06 12:40:15 +0100

    Added add to bag functionality

--- bouquets/settings.py
+++ bouquets/settings.py
@@ -110,7 +110,8 @@ WSGI_APPLICATION = 'bouquets.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")), # otherwise some errors were occurred in the terminal
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
